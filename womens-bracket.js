99563f66e4dd13107ec0713dafde687de1e995b3
21jmoore21 <131609470+21jmoore21@users.noreply.github.com>
2023-12-11 20:49:39 -0600

    Add files via upload

--- womens-bracket.js
+++ womens-bracket.js
@@ -718,6 +718,7 @@ function find_winner(){
     }
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.getElementById('readfile').value = null; // Clear File Form
 }
 
 function slider_1_val(val){
