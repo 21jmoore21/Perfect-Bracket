d22fb8b16f05efbd94f84893234ff65578e9ea4d
21jmoore21 <131609470+21jmoore21@users.noreply.github.com>
2024-01-28 21:46:09 -0600

    Add files via upload

--- mens-bracket.js
+++ mens-bracket.js
@@ -269,7 +269,7 @@ function perfect_settings() {
     let study_year = document.getElementById("yearofstudy").innerHTML;
     let current_year = 2024;
     if (study_year == 2023){
-        var slider_weights = [5, 9, -7, 10, -27, 0, -12, 80, -36, 83, 78, -1, -47, 8, 50]
+        var slider_weights = [7, 7, -33, 10, -26, 1, -15, 78, -37, 83, 77, 1, -46, 8, 50]
     }
     else if (study_year == 2022){
         var slider_weights = [-75, -24, -72, 95, 61, 35, 27, -1, -84, 96, -13, -41, 83, 93, 94]
