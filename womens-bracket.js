6bdbeee0975a719e6720e1dfb4772edc34f5ef4b
21jmoore21 <131609470+21jmoore21@users.noreply.github.com>
2024-02-11 13:48:40 -0600

    Add files via upload

--- womens-bracket.js
+++ womens-bracket.js
@@ -271,10 +271,10 @@ function perfect_settings() {
         var slider_weights = [-48, 41, 90, -42, 66, 34, 27, 94, -79, 60, 62, 49, -35, 14, 87]
     }
     else if (study_year == 2022){
-        var slider_weights = [66, 37, 34, -47, 99, -9, -43, -65, -97, 53, 28, 15, 43, -69, 95]
+        var slider_weights = [83, -94, 19, -8, 69, -83, -29, -87, 35, 13, 89, 84, 29, -33, 78]
     }
     else if (study_year == 2021){
-        var slider_weights = [5, -4, -41, 7, -6, 95, -45, -27, 82, -12, 39, 3, -3, -8, 97]
+        var slider_weights = [-67, 41, 15, -2, -4, 91, -31, -39, 71, -1, 6, -41, -5, 23, 99]
     }
     for (let j = 0, len = 15; j < len; j++) {
         let id = j+1;
